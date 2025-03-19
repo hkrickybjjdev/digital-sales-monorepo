edb1aba425d73e9f0198d158a7ebe8654e415ea9
ricky <ricky.wong@aigens.com>
2025-03-20 00:39:17 +0800

    refactor(builder): Simplify API route definitions for content blocks and page settings
    - Removed schema validation from route definitions for content blocks and page settings.
    - Streamlined the POST and PUT routes to directly call the respective handler functions.

--- apps/api/src/modules/builder/index.ts
+++ apps/api/src/modules/builder/index.ts
@@ -6,7 +6,6 @@ import { Env } from '../../types';
 // Import handlers
 import * as contentBlockHandlers from './controllers/contentBlockHandlers';
 import * as pageSettingsHandlers from './controllers/pageSettingsHandlers';
-import { predefinedContentBlockSchema, pageSettingsDefinitionSchema } from './models/schemas';
 
 // Create the builder module router
 const builderModule = new Hono<{ Bindings: Env }>();
@@ -14,16 +13,8 @@ const builderModule = new Hono<{ Bindings: Env }>();
 // Predefined Content Block Routes
 builderModule.get('/content-blocks', contentBlockHandlers.getAllContentBlocks);
 builderModule.get('/content-blocks/:type', contentBlockHandlers.getContentBlockByType);
-builderModule.post(
-  '/content-blocks',
-  zValidator('json', predefinedContentBlockSchema),
-  contentBlockHandlers.createContentBlock
-);
-builderModule.put(
-  '/content-blocks/:type',
-  zValidator('json', predefinedContentBlockSchema),
-  contentBlockHandlers.updateContentBlock
-);
+builderModule.post('/content-blocks', contentBlockHandlers.createContentBlock);
+builderModule.put('/content-blocks/:type', contentBlockHandlers.updateContentBlock);
 builderModule.delete('/content-blocks/:type', contentBlockHandlers.deleteContentBlock);
 builderModule.get(
   '/content-blocks/category/:category',
@@ -34,16 +25,8 @@ builderModule.get('/content-blocks/public', contentBlockHandlers.getPublicConten
 // Page Settings Definition Routes
 builderModule.get('/page-settings', pageSettingsHandlers.getAllPageSettings);
 builderModule.get('/page-settings/:name', pageSettingsHandlers.getPageSettingByName);
-builderModule.post(
-  '/page-settings',
-  zValidator('json', pageSettingsDefinitionSchema),
-  pageSettingsHandlers.createPageSetting
-);
-builderModule.put(
-  '/page-settings/:name',
-  zValidator('json', pageSettingsDefinitionSchema),
-  pageSettingsHandlers.updatePageSetting
-);
+builderModule.post('/page-settings', pageSettingsHandlers.createPageSetting);
+builderModule.put('/page-settings/:name', pageSettingsHandlers.updatePageSetting);
 builderModule.delete('/page-settings/:name', pageSettingsHandlers.deletePageSetting);
 builderModule.get(
   '/page-settings/category/:category',
